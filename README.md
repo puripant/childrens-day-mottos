a98579b3b344a77e077935a6f9910e4fb0178548
Puripant <puripant@gmail.com>
2019-12-14 12:23:33 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Children's Day Mottos in Thailand
+Thai prime ministers give a motto on the National Children's Day every year. It can reflect the social mentality and priority at the time.
+
+![Screenshot](thumbnail.png)
---
+++ main.js
@@ -0,0 +1,19 @@
+const wordcut = require("wordcut");
+wordcut.init();
+
+// const split_words = words
+//   .map(w => wordcut.cut(w).split("|"))
+//   .filter(a => a.length == 2);
+// // console.log(split_words.length);
+
+// const funny_words = split_words
+//   .filter(w => nouns.indexOf(w[0]) > -1 && verbs.indexOf(w[1]) > -1);
+// // console.log(funny_words);
+
+// const noun = document.getElementById("noun");
+// const verb = document.getElementById("verb");
+// function changeWord() {
+//   let word = funny_words[Math.floor(Math.random()*funny_words.length)];
+//   noun.innerHTML = word[0];
+//   verb.innerHTML = word[1];
+// }
