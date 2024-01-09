329b56a7c043e8f661d5263ffeea1062c66946f0
Puripant R <puripantr@gmail.com>
2024-01-09 22:00:42 +0700

    Add new data and transition

--- main.js
+++ main.js
@@ -3,21 +3,20 @@ wordcut.init();
 
 const header = d3.select("#header");
 const motto = d3.select("#motto");
-const t = motto.transition().duration(2000);
 
 d3.csv("data.csv").then(data => {
-  let freqs = {};
+  // let freqs = {};
   for (let i = 0; i < data.length; i++) {
     data[i].words = wordcut.cut(data[i].motto).split("|");
-    data[i].words.forEach(word => {
-      if (freqs.hasOwnProperty(word)) {
-        freqs[word]++;
-      } else {
-        freqs[word] = 1;
-      }
-    });
+    // data[i].words.forEach(word => {
+    //   if (freqs.hasOwnProperty(word)) {
+    //     freqs[word]++;
+    //   } else {
+    //     freqs[word] = 1;
+    //   }
+    // });
   }
-  console.log(freqs);
+  // console.log(freqs);
 
   let counter = 0;
   let update = () => {
@@ -31,16 +30,24 @@ d3.csv("data.csv").then(data => {
       .join(
         enter => enter.append("span")
           .classed("word", true)
-          .classed("new", true)
-          .text(d => d),
-          // .call(enter => enter.transition(t).attr("y", 0)),
+          .text(d => d)
+          .style("color", "rgb(235, 48, 124)")
+          .style("opacity", 0)
+          .style("letter-spacing", "-5em")
+          .call(enter => enter.transition().duration(2000).delay(500)
+            .style("opacity", 1)
+            .style("letter-spacing", "0")
+          ),
         update => update
-          .classed("new", false),
-          // .call(update => update.transition(t).attr("x", (d, i) => i * 16)),
+          .call(update => update.transition().duration(1000)
+            .style("color", "black")
+          ),
         exit => exit
-          .classed("old", true)
-          .remove()
-          // .call(exit => exit.transition(t).style("opacity", 0).remove())
+          .call(exit => exit.transition().duration(1000)
+            .style("opacity", 0)
+            .style("letter-spacing", "-1em")
+            .remove()
+          )
       );
 
     counter++;
